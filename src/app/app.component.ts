16ea9238b16c305309de248828096a4b44d45143
ElNeko42 <franson177@gmail.com>
2023-07-19 19:35:42 +0200

    modificacion index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScryfallService } from './scryfall.service'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mtgcard';
+  cards: any[] = [];
+
+  constructor(private scryfallService: ScryfallService) { }
+
+  ngOnInit() {
+    this.scryfallService.getCards().subscribe(data => {
+      this.cards = data.data.slice(0, 5);
+    });
+  }
 }
--- src/app/scryfall.service.ts
+++ src/app/scryfall.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScryfallService {
 
-  constructor() { }
+  private baseUrl = 'https://api.scryfall.com';
+
+  constructor(private http: HttpClient) { }
+
+  getCards(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/cards/search?q=c%3Awhite+pow%3D3`);
+  }
 }
